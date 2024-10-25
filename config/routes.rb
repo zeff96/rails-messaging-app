ce3ea1a55e0ff9529e90533b9a3e27866ccc579e
zeff96 <zeffo1975@gmail.com>
2024-10-26 00:56:34 +0300

    Add resourceful routes for notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "notifications/index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -14,4 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "posts#index"
   resources :posts
+  resources :notifications
 end
