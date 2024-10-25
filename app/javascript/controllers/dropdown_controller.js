d2bb105302e11243282fd5c16ab05ecb0491c35a
zeff96 <zeffo1975@gmail.com>
2024-10-25 16:30:26 +0300

    Add dropdown stimulus for toggle user profile dropdown

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["menu"];
+  toggleMenu() {
+    this.menuTarget.classList.toggle("hidden");
+  }
+}
