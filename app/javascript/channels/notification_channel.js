2fe87270434b53b71cc9247904f7f683fa12f64f
zeff96 <zeffo1975@gmail.com>
2024-10-29 12:16:50 +0300

    Add notification channel to current

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -1,15 +1,34 @@
-import consumer from "channels/consumer"
+import consumer from "./consumer";
 
-consumer.subscriptions.create("NotificationChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+const notificationChannel = consumer.subscriptions.create(
+  "NotificationChannel",
+  {
+    connected() {
+      console.log("Connected to the notification channel");
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      console.log("Disconnected from the notification channel");
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    received(data) {
+      // This method will be called whenever a new notification is received
+      const notification = {
+        url: data.url,
+        actor: data.actor,
+        action: data.action,
+        notifiable: data.notifiable,
+      };
+
+      // Call the Stimulus controller method to update the UI
+      const notificationController = document.querySelector(
+        "[data-controller='notifications']"
+      ).controller;
+
+      notificationController.updateNotification([notification]);
+      notificationController.updateCount(
+        notificationController.countTarget.innerHTML + 1
+      );
+    },
   }
-});
+);
