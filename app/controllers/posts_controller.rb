172ddcf78694664fd9ffdbc53b0129a1b3035831
zeff96 <zeffo1975@gmail.com>
2024-10-29 12:16:07 +0300

    Update post order to be loaded in descending order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
   before_action :set_post, only: [ :show ]
   def index
-    @posts = Post.includes(:user).all
+    @posts = Post.includes(:user).all.order(created_at: :desc)
   end
 
   def show; end
