b3a8d9f8c4ad42a3a10f96ea3539e99c1a6b3d47
zeff96 <zeffo1975@gmail.com>
2024-11-09 20:36:23 +0300

    Update notifications controller and remove methods for loading and update notifications

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -3,34 +3,9 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["menu", "count"];
 
-  connect() {
-    this.loadNotifications();
-  }
-
   toggle() {
     this.menuTarget.classList.toggle("hidden");
     this.updateNotificationRead();
-  }
-
-  loadNotifications() {
-    fetch("/notifications.json")
-      .then((response) => response.json())
-      .then((data) => {
-        this.updateNotification(data);
-        this.updateCount(data.length);
-      })
-      .catch((error) => console.error(error.message));
-  }
-
-  updateNotification(notifications) {
-    notifications.forEach((notification) => {
-      const newNotification = `<a class="block text-sm text-gray-700" href=${notification.url}>${notification.actor} ${notification.action} ${notification.notifiable.type}</a>`;
-      this.menuTarget.insertAdjacentHTML("beforeend", newNotification);
-    });
-  }
-
-  updateCount(length) {
-    this.countTarget.innerHTML = length;
   }
 
   updateNotificationRead() {
