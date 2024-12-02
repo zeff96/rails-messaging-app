2e1cd62afcaf102eccb3bf88a23f6f07ff8f3f5b
zeff96 <zeffo1975@gmail.com>
2024-12-02 13:36:38 +0300

    Add method to set notification counts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_unread_notifications
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :image ])
+  end
+
+  private
+
+  def set_unread_notifications
+    if user_signed_in?
+      @unread_notifications_count = current_user.notifications.unread.count
+    end
   end
 end
